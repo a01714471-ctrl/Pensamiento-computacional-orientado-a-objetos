00025e79493cc073468146b1706b91dc57eb5523
a01714471-ctrl <a01714471@tec.mx>
2025-11-13 19:27:40 -0600

    Código principal

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Jugador.h"
+#include "Equipo.h"
+#include "Balon.h"
+
+using namespace std;
+
+int main() {
+    // Crear objetos
+    Jugador jugador("Jordan", 23, 1.98);
+    Equipo equipo("Chicago Bulls", "Chicago");
+    Balon pelota(7.5, "cuero");
+
+    cout << "INFORMACION INICIAL\n";
+    jugador.showInfo();
+    equipo.showTeam();
+    pelota.bounce();
+
+    // Modificar algunos atributos
+    jugador.setName("LeBron James");
+    equipo.setTeamName("Los Angeles Lakers");
+    pelota.setMaterial("sintético");
+
+    cout << "\nINFORMACION MODIFICADA\n";
+    jugador.showInfo();
+    equipo.showTeam();
+    pelota.bounce();
+}
